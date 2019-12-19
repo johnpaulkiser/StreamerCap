abf6ac839d0830dcebe8a544f7f55e7e7675cb79
JP <johnpaulkiser@gmail.com>
2019-12-19 12:03:58 -0500

    Create 'hello world'

--- streamercap/streamercap/urls.py
+++ streamercap/streamercap/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rankings.views import landing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing, name='landing')
 ]
