1ac33872db9c04120a00a4ab7345543935bcadb8
JP <johnpaulkiser@gmail.com>
2019-12-21 14:15:52 -0500

    Display dummy data in table

--- streamercap/rankings/views.py
+++ streamercap/rankings/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from rankings.models import LiveSession
 
 
 
 def landing(request):
-    return render(request, 'index.html')
+    currently_live = LiveSession.objects.filter(is_live=True).order_by('-current_viewers')
+    print(currently_live)
+    context = {
+        'streamers': currently_live,
+    }
+    return render(request, 'index.html', context=context)
